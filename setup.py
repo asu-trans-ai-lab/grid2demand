b5f9236bd5d93c99ae375ede60dcd74abf699ec6
xyluo25 <luoxiangyong01@gmal.com>
2024-09-22 15:48:01 -0700

    v0.6.4

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ except Exception:
 
 setuptools.setup(
     name="grid2demand",  # Replace with your own username
-    version="0.6.3rc4",
+    version="0.6.4",
     author="Xiangyong Luo, Dr.Xuesong(Simon) Zhou",
     author_email="luoxiangyong01@gmail.com, xzhou74@asu.edu",
     description="A tool for generating zone-to-zone travel demand based on grid cells or TAZs and gravity model",
